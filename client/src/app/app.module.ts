84fbcccd1c1a2b991a0b0e5644d4f7dbfd1aff67
nachaphonchoto <nachaphonchoto@gmail.com>
2022-01-09 20:42:34 +0700

    primeNG

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion';     
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
