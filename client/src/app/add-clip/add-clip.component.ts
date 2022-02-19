2437538837596a06a229bf49ceb8c83de16d2667
nachaphonchoto <nachaphonchoto@gmail.com>
2022-02-19 20:35:19 +0700

    config

--- client/src/app/add-clip/add-clip.component.ts
+++ client/src/app/add-clip/add-clip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClipApiService } from '../clip-api.service';
 
 @Component({
   selector: 'app-add-clip',
@@ -7,20 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddClipComponent implements OnInit {
 
-  title: any;
-
-  uploader: any;
-
-  description: any;
-
+  title: string | undefined;
+  uploader: string | undefined;
+  description: string | undefined;
   url: string | undefined;
+  rating: number = 0;
 
-  rating: number | undefined;
 
-
-  constructor() { }
+  constructor(private clipApiService:ClipApiService ) { }
 
   ngOnInit(): void {
+  }
+
+  addClip(): void {
+    
+    var val = {
+      title: this.title,
+      uploader: this.uploader,
+      description: this.description,
+      url: this.url,
+      rating: this.rating,
+    }
+    
+    this.clipApiService.addClip(val).subscribe(res => {
+      alert(res.toString());
+    });
   }
 
 }
--- server/database/filters.py
+++
@@ -1,9 +0,0 @@
-
-import django_filters
-from .models import Clip
-
-class Clipfilter(django_filters.FilterSet):
-
-    class Meta:
-        model = Clip
-        fields = ['title', 'uploader', 'description']
