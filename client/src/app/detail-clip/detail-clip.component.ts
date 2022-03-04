122e895b93a47381f4683d455fc8e2ad1145debf
tanawat <pirotover1@gmail.com>
2022-03-04 19:51:19 +0700

    change

--- client/src/app/detail-clip/detail-clip.component.ts
+++ client/src/app/detail-clip/detail-clip.component.ts
@@ -10,23 +10,11 @@ import { ClipApiService } from '../clip-api.service';
   styleUrls: ['./detail-clip.component.css']
 })
 export class DetailClipComponent implements OnInit {
-<<<<<<< HEAD
-  clip: any;
-  @Input() Id?: any;
-=======
->>>>>>> a8ada4521248d6cc689bb0ed2b6543116e23afbc
 
   constructor(private clip_api_service : ClipApiService) { }
 
   ngOnInit(): void {
 
-<<<<<<< HEAD
-  getClip(): void{
-    console.log(this.Id);
-    this.clip_api_service.getClip(this.slide.id)
-    .subscribe(clip => this.clip = clip);
-=======
->>>>>>> a8ada4521248d6cc689bb0ed2b6543116e23afbc
   }
   
 
